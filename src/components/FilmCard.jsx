74274a8fe909144f4eb749314a35a572e25763c3
itsneedtoknow <letta_in_love@mail.ru>
2026-06-11 12:05:33 +0300

    refactor: delete extra code

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -1,18 +1,22 @@
 import styles from "./FilmCard.module.css";
 import { Link } from "react-router";
 
-export function FilmCard() {
+export function FilmCard({ filmItem }) {
   return (
     <li className={styles.film__item}>
-      <div className={styles.film__img}></div>
+      <div className={styles.film__img}>
+        <Link to={filmItem.imdbID}>
+          <img src={filmItem.Images[0]} />
+        </Link>
+      </div>
       <div className={styles.film__content}>
         <h3 className={styles.film__title}>
-          <Link to="">Title</Link>
+          <Link to={filmItem.imdbID}>{filmItem.Title}</Link>
         </h3>
-        <p className={styles.film__description}>description</p>
-        <a href="" className={styles.film__readmore}>
+        <p className={styles.film__description}>{filmItem.Plot}</p>
+        <Link to={filmItem.imdbID} className={styles.film__readmore}>
           read more
-        </a>
+        </Link>
       </div>
     </li>
   );
