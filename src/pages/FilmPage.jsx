0dcbd93c316d769cb0c489b6e03d700c57ffd673
itsneedtoknow <letta_in_love@mail.ru>
2026-06-22 17:02:09 +0300

    refactor: add lazy loading images

--- src/pages/FilmPage.jsx
+++ src/pages/FilmPage.jsx
@@ -19,7 +19,7 @@ export function FilmPage() {
   return (
     <div className={styles.page}>
       <div className={styles.poster}>
-        <img src={film.Poster} alt={film.Title} />
+        <img src={film.Poster} alt={film.Title} loading="lazy" />
       </div>
       <div className={styles.info}>
         <h1 className={styles.title}>{film.Title}</h1>
